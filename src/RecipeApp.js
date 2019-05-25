a4ed416b8a9d9f7c165f941d2f7be5b0df46d61e
jkamz <jkamz6432@gmail.com>
2019-05-25 20:19:03 +0300

    add recipe component

---
+++ src/RecipeApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import Recipe from "./Recipe";
+import "./RecipeApp.css";
+
+function RecipeApp() {
+  return (
+    <div className="App">
+      <Recipe />
+    </div>
+  );
+}
+
+export default RecipeApp;
